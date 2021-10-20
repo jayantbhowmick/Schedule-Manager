b642f386d1beebee053e44e47f3d4a670e9223eb
jayantb20 <thrustydemon@gmail.com>
2021-10-20 18:44:35 +0530

    learnt abou toLocaleDateString() and coustomize the rendering.

--- app.js
+++ app.js
@@ -3,39 +3,28 @@ const bodyParser = require("body-parser");
 
 const port = 3000;
 const app = express();
-var date = new Date();
-var today = date.getDay();
-var day = "";
+
+var today = new Date();
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  switch (today) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default: console.log("some error!!");
-      
-  }
-  res.render("list", { kindOfDay: day }); 
+  
+  var options = { weekday:'long',
+                  day: 'numeric',
+                  month:'long',
+                  year:"numeric"};
+
+// using the Date() functions created an object and used toLocaleDateString() to format the conventions and coustomize the rendering.              
+
+
+
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", { 
+    kindOfDay: day 
+  });
+
 });
 
 app.listen(port, () => console.log(`listening to port ${port} and ${today}`));
