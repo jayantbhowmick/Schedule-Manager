8ea10f4963d72b99bc754107cf4968e4f3d224c7
jayantb20 <thrustydemon@gmail.com>
2021-10-21 20:12:25 +0530

    added premade css and added class names

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const port = 3000;
 const app = express();
-var items = ["Buy Food", "Cook Food", "Eat Food" ];
-var today = new Date();
+const port = 3000;
+let today = new Date();
+let items = ["Buy Food","Cook Food","Eat Food"];
+
 
 app.set("view engine", "ejs");
 
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(express.urlencoded({extended:false}))
 app.use(express.json());
+app.use(express.static("public"))
 
-app.get("/", (req, res) => {
-  
-  var options = { weekday:'long',
-                  day: 'numeric',
-                  month:'long',
-                  year:"numeric"};
+app.get("/", (req,res) => {
 
-// using the Date() functions created an object and used toLocaleDateString() to format the conventions and coustomize the rendering.              
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    year: "numeric",
+    month:"long"
+  }
+// using Date() created an object and used toLocaleDateString() to format the conventions and coustomize the rendering.
+
+  let day = today.toLocaleDateString("en-US", options)
+
+  res.render("list", {kindOfDay: day, newListItems:items
+  }) // When the webpage is tring to render list at this point it doesn't have any value for new list item, every single time we want to render list we have to provide values for all the variables.
+}) 
 
 
 
-    var day = today.toLocaleDateString("en-US", options);
-
-    res.render("list", { 
-    kindOfDay: day, newListItems:items
-  }); // When the webpage is trying to render list at this point it doesn't have any value for new list item,every single time we want to render list we have to provide values for all the variables 
-
-});
-
-app.post("/", (req,res) => {
-   var item = req.body.newItem; // This allows us to get hold of the data we entered in the newItem.
+app.post("/",(req,res) => {
+  let item = req.body.newItem; // this allows us to get hold of the data we entered in the newItem input
   items.push(item);
-   res.redirect("/");
+  res.redirect("/");
 })
 
-app.listen(port, () => console.log(`listening to port ${port} and ${today}`));
+app.listen(port, () => console.log( `listening on port ${port} `));
